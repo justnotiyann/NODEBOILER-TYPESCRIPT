9a315dfa9f7003bc0d02769caa2bfc39b1cd2e78
bang_iyan <muhammadfitrian0712gmail.com>
2023-06-21 00:12:51 +0700

    Progress

--- dist/src/api/users/users.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const users_controller_1 = require("./users.controller");
-const router = (0, express_1.Router)();
-router.get("/", users_controller_1.get);
-exports.default = router;
--- dist/src/middleware/handleErrorMiddleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function notFoundMiddleware(error, req, res, next) {
-    res.status(400).json({
-        data: error,
-    });
-}
-exports.default = notFoundMiddleware;
